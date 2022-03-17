b055e96668ace9f3e31dccc90aa6cc0ac89018b0
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2022-03-17 14:06:59 -0400

    Created import method

--- app/controllers/api/v1/catalogs_controller.rb
+++ app/controllers/api/v1/catalogs_controller.rb
@@ -1,7 +1,15 @@
 module Api
   module V1
     class CatalogsController < ApplicationController
-      def index; end
+      def index
+      @catalogs = Catalog.all
+      render json: @catalogs.to_json(only: %i[id title genre release_year country date_added description]), status: :ok
+      end
+
+      def import
+        Catalog.import_from_csv(params[:file])
+        redirect_to api_v1_catalogs_path, notice: "Activity Data Imported"
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resource :catalogs, only: [:index]
+      resource :catalogs do
+        collection do
+          get :index
+          post :import
+        end
+      end
     end
   end
 end
