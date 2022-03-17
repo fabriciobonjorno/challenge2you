be69a8d06cae8efcc1fcd32ee72bebe076aa722b
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2022-03-17 17:58:02 -0400

    Refactored service import catalog

--- app/services/import_catalogs_service.rb
+++ app/services/import_catalogs_service.rb
@@ -16,33 +16,40 @@ class ImportCatalogsService
     CSV.foreach(@file.path, headers: true) do |row|
       row = row.to_hash
       begin
-        import_catalog = Catalog.find_or_create_by!(
-          show_id: row['show_id'],
-          genre: row['type'],
-          title: row['title'],
-          director: row['director'],
-          cast: row['cast'],
-          country: row['country'],
-          date_added: row['date_added'].to_date,
-          release_year: row['release_year'].to_i,
-          rating: row['rating'],
-          duration: row['duration'],
-          listed_in: row['listed_in'],
-          description: row['description']
-        )
+        create_catalog(row)
       rescue StandardError => e
         not_imported << "#{row['title']} not imported, reason: " + e.to_s
-      end
+      end      
+      create_log(not_imported)
+    end
+  end
 
-      path = Rails.root + 'log/not_imported.log'
-      FileUtils.rm_rf(path) if File.exist?(path)
-      File.open(path, 'wb') do |f|
-        not_imported.each do |row|
-          f.write(row)
-          f.write "\r\n"
-        end
-        f.close
+  def create_catalog(row)
+    import_catalog = Catalog.find_or_create_by!(
+      show_id: row['show_id'],
+      genre: row['type'],
+      title: row['title'],
+      director: row['director'],
+      cast: row['cast'],
+      country: row['country'],
+      date_added: row['date_added'].to_date,
+      release_year: row['release_year'].to_i,
+      rating: row['rating'],
+      duration: row['duration'],
+      listed_in: row['listed_in'],
+      description: row['description']
+    )
+  end
+
+  def create_log(not_imported)
+    path = Rails.root + 'log/not_imported.log'
+    FileUtils.rm_rf(path) if File.exist?(path)
+    File.open(path, 'wb') do |f|
+      not_imported.each do |row|
+        f.write(row)
+        f.write "\r\n"
       end
+      f.close
     end
   end
 end
